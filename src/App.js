26fc04737550d0d0f20d6a0a0d926bb877be563e
Albert Kim <albert275@gmail.com>
2019-07-29 22:36:20 -0700

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <h1 className='display-4'>The Herald</h1>
+      <br />
+      <div className='d-flex'>
+        <div className='mr-2' style={{flexGrow: 1}}>
+          <h2>This is breaking news!</h2>
+          <div><i>People do things</i></div>
+          <div><i>Jonathan Bonder</i></div>
+          <hr />
+          <div>This is the content</div>
+        </div>
+        <div className='border border-light p-4' style={{width: 300}}>
+          <div>Title 1</div>
+          <div>Title 2</div>
+          <div>Title 3</div>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
